ed315c9fe151d6a1e87bb3763aca0d46e9208ebe
Iskandar <iskandarfazliddinov48@gmail.com>
2023-05-16 05:21:11 +0500

    feat: add profile_screen appBar

--- lib/presentation/ui/screens/profile/profile_screen.dart
+++ lib/presentation/ui/screens/profile/profile_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:student_com_app/presentation/ui/resources/app_color.dart';
+import 'package:student_com_app/presentation/ui/resources/app_style.dart';
 
 
 class ProfileScreen extends StatefulWidget {
@@ -11,6 +13,39 @@ class ProfileScreen extends StatefulWidget {
 class _ProfileScreenState extends State<ProfileScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return  Scaffold(
+      appBar: _getAppBar(),
+    );
   }
+  _getAppBar() => AppBar(
+    backgroundColor: AppColors.appBarColor,
+    actions: [
+      SizedBox(
+        width: MediaQuery.of(context).size.width,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(left: 12.0),
+              child: Row(
+                children: [
+                  Icon(Icons.person_outline_sharp,color: Colors.white.withOpacity(0.8,),),
+                  Text("I'm a partner",style: TextStyle(color: Colors.white.withOpacity(0.8),fontSize: 16),)
+                ],
+              ),
+            ),
+            Text("Profile",style: AppStyles.getTitleCategory().copyWith(color: Colors.white,fontSize: 20),),
+            const SizedBox(width: 4),
+            const Padding(
+              padding: EdgeInsets.only(right: 12.0),
+              child: Icon(
+                Icons.headset_mic,
+                color: Colors.white,
+              ),
+            ),
+          ],
+        ),
+      )
+    ],
+  );
 }
