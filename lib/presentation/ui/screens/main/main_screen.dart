9eb7ad9ddc451f10efaed5f06282a8d3ae34e590
Iskandar <iskandarfazliddinov48@gmail.com>
2023-05-18 22:47:42 +0500

    fixed: bug in bottomNavigation.

--- lib/presentation/ui/screens/main/main_screen.dart
+++ lib/presentation/ui/screens/main/main_screen.dart
@@ -12,67 +12,93 @@ class MainScreen extends StatefulWidget {
   State<MainScreen> createState() => _MainScreenState();
 }
 
-class _MainScreenState extends State<MainScreen>
-    with SingleTickerProviderStateMixin {
-  late TabController tabController;
+class _MainScreenState extends State<MainScreen> {
+  int currentTabIndex = 0;
+  late PageController _pageController;
+  late List<Widget> _pages;
 
   @override
   void initState() {
+    // TODO: implement initState
     super.initState();
-    tabController =
-        TabController(length: 4, vsync: this, initialIndex: currentTabIndex);
+    _pageController = PageController(initialPage: currentTabIndex);
+    _pages = [
+      const HomeScreen(),
+      const MapScreen(),
+      const BlogScreen(),
+      const ProfileScreen()
+    ];
+  }
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
   }
 
-  int currentTabIndex = 0;
+  void _onPageChanged(int index) {
+    setState(() {
+      currentTabIndex = index;
+    });
+  }
+  void _onItemTapped(int index) {
+    _pageController.animateToPage(
+      index,
+      duration: const Duration(milliseconds: 300),
+      curve: Curves.ease,
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: TabBarView(
-        controller: tabController,
-        children: const [
-          HomeScreen(),
-          MapScreen(),
-          BlogScreen(),
-          ProfileScreen()
-        ],
+      body: PageView(
+        controller: _pageController,
+        onPageChanged: _onPageChanged,
+        children: _pages,
       ),
       bottomNavigationBar: Container(
-        decoration: const BoxDecoration(color: Colors.white),
-        height: 80,
-        child: TabBar(
-          indicatorColor: Colors.transparent,
-          controller: tabController,
-          onTap: (index) {
-            setState(() {
-              currentTabIndex = index;
-            });
-          },
-          tabs: [
-            Image.asset(
-              currentTabIndex == 0 ? AppIcons.homeSelect : AppIcons.home,
-              width: 20,
-              height: 20,
-            ),
-            Image.asset(
-              currentTabIndex == 1
-                  ? AppIcons.placeholderSelect
-                  : AppIcons.placeholder,
-              width: 20,
-              height: 20,
-            ),
-            Image.asset(
-              currentTabIndex == 2 ? AppIcons.blogSelect : AppIcons.blog,
-              width: 20,
-              height: 20,
-            ),
-            Image.asset(
-              currentTabIndex == 3 ? AppIcons.userSelect : AppIcons.user,
-              width: 20,
-              height: 20,
-            ),
-          ],
-        ),
+          decoration: const BoxDecoration(color: Colors.white),
+          height: 80,
+          child: BottomNavigationBar(
+            currentIndex: currentTabIndex,
+            onTap: _onItemTapped,
+            items: [
+              BottomNavigationBarItem(
+                icon: Image.asset(
+                  currentTabIndex == 0 ? AppIcons.homeSelect : AppIcons.home,
+                  width: 20,
+                  height: 20,
+                ),
+                  label: ""
+              ),
+              BottomNavigationBarItem(
+                icon: Image.asset(
+                  currentTabIndex == 1
+                      ? AppIcons.placeholderSelect
+                      : AppIcons.placeholder,
+                  width: 20,
+                  height: 20,
+                ),
+                  label: ""
+              ),
+              BottomNavigationBarItem(
+                icon: Image.asset(
+                  currentTabIndex == 2 ? AppIcons.blogSelect : AppIcons.blog,
+                  width: 20,
+                  height: 20,
+                ),
+                  label: ""
+              ),
+              BottomNavigationBarItem(
+                icon: Image.asset(
+                  currentTabIndex == 3 ? AppIcons.userSelect : AppIcons.user,
+                  width: 20,
+                  height: 20,
+                ),
+                label: ""
+              )
+            ],
+          )
       ),
     );
   }
